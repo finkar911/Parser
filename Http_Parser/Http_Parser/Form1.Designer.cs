c4bb8db574fbb32c0cd87b45bdcf3a33928cf329
finkar911 <graf_132@mail.ru>
2016-09-10 20:14:46 +0400

    V3

---
+++ Http_Parser/Http_Parser/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace Http_Parser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 74);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(204, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Сформировать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(12, 28);
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(92, 20);
+            this.numericUpDown1.TabIndex = 1;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(125, 28);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(82, 20);
+            this.numericUpDown2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(122, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Фильтр уровня";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Первая страница";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(-1, 130);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(208, 23);
+            this.progressBar1.Step = 1;
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar1.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Обработанно:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(0, 56);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(158, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Количество стр. для анализа:";
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(160, 52);
+            this.numericUpDown3.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(47, 20);
+            this.numericUpDown3.TabIndex = 8;
+            this.numericUpDown3.Value = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(209, 6);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(251, 148);
+            this.listBox1.TabIndex = 9;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(464, 19);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(137, 134);
+            this.listBox2.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(464, 3);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(50, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Фильтр:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(209, 156);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Http Parser";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Http_Parser/Http_Parser/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Http_Parser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //List<string> code = new List<string>();
+        int divonstr = 12;
+        string code;
+        //public string[] newpage;
+        public int sinh = 0;
+        public int turn = 1;
+        //public List<string> filter = new List<string>();
+        string[] filter=File.ReadAllLines(@"filter.txt");
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            for (int i = 0; i < filter.Length; i++) {
+                listBox2.Items.Add(filter[i]);
+            }
+            //int progbar = 0;
+            // filter = 
+            turn = 1;
+            button1.Enabled = false;
+            numericUpDown1.Enabled = false;
+            numericUpDown2.Enabled = false;
+            numericUpDown3.Enabled = false;
+            timer1.Enabled = true;
+            //newpage = new string[Convert.ToInt32(numericUpDown3.Value)];
+            progressBar1.Value = 0;
+            progressBar1.Maximum = divonstr * Convert.ToInt32(numericUpDown3.Value)+(Convert.ToInt32(numericUpDown3.Value));
+            label3.Text = "Нужно проанализировать "+ progressBar1.Maximum.ToString() + " пунктов";
+            code = "<html><body>";
+            sinh = Convert.ToInt32(numericUpDown1.Value);
+            //parser();
+
+            for (int ii = Convert.ToInt32(numericUpDown1.Value); ii < Convert.ToInt32(numericUpDown1.Value)+Convert.ToInt32(numericUpDown3.Value); ii++)
+            {
+                Thread myThread = new Thread(new ParameterizedThreadStart(trhreadParser));
+                myThread.Start(ii);
+            }
+
+            //code += "</body></html>";
+            //string Date = DateTime.Now.ToString();
+            //Date= Date.Replace(":", " ");
+            //File.WriteAllText(Date+".html", code, Encoding.Default);
+
+        }
+
+            void trhreadParser(object ia) {
+            string htmtoout="";
+            int ii = (int)ia;
+            var webClient = new System.Net.WebClient();
+            string HTML = webClient.DownloadString("https://www.indiegala.com/giveaways/" + ii + "/expiry/asc/level/0");
+            HTML = HTML.Substring(HTML.IndexOf("row tickets-row"), HTML.IndexOf("</section>", HTML.IndexOf("row tickets-row")) - HTML.IndexOf("row tickets-row"));
+
+            //this.BeginInvoke(new Action(delegate ()
+            //{
+            //    listBox1.Items.Add("Поток "+ii+" закончил загрузку страницы");
+               
+            //}));
+           
+
+            int fi = 0;
+            int li = 0;
+            int fip = 0;
+            int lip = 0;
+            for (int i = 0; i < divonstr; i++)
+            {
+                //this.BeginInvoke(new Action(delegate ()
+                //{
+                //    progressBar1.Value = progressBar1.Value + 1;
+
+                //}));
+                //progressBar1.Invoke(progressBar1.Value++);
+                int est = 0;
+               
+
+                fi = HTML.IndexOf("<h2>", fi + 5);
+                li = HTML.IndexOf("</h2>", li + 5)+5 ;//+5
+                fip = HTML.IndexOf("<strong>", fip + 5);
+                lip = HTML.IndexOf("</strong>", lip + 5) + 5;
+                string add = HTML.Substring(fi, li - fi);
+                for (int iai = 0; iai < filter.Length; iai++)
+                {
+                    if (add.Contains(filter[iai]))
+                    {
+                        est = 1;
+                    }
+                }
+                if (est==0){
+                    string firspart = add.Substring(4, add.IndexOf("<a href=") + 5);
+                    string lastpart = "https://www.indiegala.com" + add.Substring(add.IndexOf("<a href=") + 9,add.IndexOf("</h2>")- (add.IndexOf("<a href=")+9));//+9
+
+                    //fip = HTML.IndexOf("<strong>", fip + 5);
+                    //lip = HTML.IndexOf("</strong>", lip + 5) + 5;
+                    string ip = HTML.Substring(fip + 8, lip - (fip + 8)) + "<br><br>";
+
+                    
+
+                    htmtoout += firspart + lastpart + " IP ="+ip;
+                   // break;
+                }
+            }
+            this.BeginInvoke(new Action(delegate ()
+            {
+                progressBar1.Value = progressBar1.Value + 6;
+                listBox1.Items.Add("Поток " + ii + " закончил анализ страницы и ждет своей очереди для записи");
+
+            }));
+            for (int i = 0; i < 50; i--)
+            {
+                if (ii == sinh)
+                {
+                    this.BeginInvoke(new Action(delegate ()
+                    {
+                        //listBox1.Items.Add(htmtoout);
+                        code += htmtoout;
+                    }));
+                    int change = ii + 1;
+                    int ch = turn + 1;
+                    Interlocked.Exchange(ref sinh, change);
+                    Interlocked.Exchange(ref turn, ch);
+                    this.BeginInvoke(new Action(delegate ()
+                    {
+                        listBox1.Items.Add("Поток " + ii + " закончил работу.");
+
+                        progressBar1.Value = progressBar1.Value + 7;
+                        
+                    }));
+                    i = 55;
+                    break;
+                    
+                }
+                else {
+                    Thread.Sleep(500);
+                }
+            }
+            
+
+        }
+        //static void threadout(out List<string> newpage) {
+
+        //    //newpage = "11";
+        //}
+
+        public void parser(){
+            var webClient = new System.Net.WebClient();
+            for (int ii = Convert.ToInt32(numericUpDown1.Value); ii <= Convert.ToInt32(numericUpDown3.Value); ii++)
+            {
+                string HTML = webClient.DownloadString("https://www.indiegala.com/giveaways/" + ii + "/expiry/asc/level/" + numericUpDown2.Value.ToString());
+                HTML = HTML.Substring(HTML.IndexOf("row tickets-row"), HTML.IndexOf("</section>", HTML.IndexOf("row tickets-row")) - HTML.IndexOf("row tickets-row"));
+                int fi = 0;
+                int li = 0;
+                for (int i = 0; i < 12; i++)
+                {
+                    progressBar1.Value++;
+                    fi = HTML.IndexOf("<h2>", fi + 5);
+                    li = HTML.IndexOf("</h2>", li + 5) + 5;
+                    string add = HTML.Substring(fi, li - fi);
+                    if (!add.Contains("Savage Resurrection"))
+                    {
+                        string firspart = add.Substring(0, add.IndexOf("<a href=") + 9);
+                        string lastpart = "https://www.indiegala.com" + add.Substring(add.IndexOf("<a href=") + 9);
+                        code += firspart + lastpart;
+                    }
+                }
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //progressBar1.Value = (turn - 1) * divonstr;
+            label3.Text = "Обработанно "+(turn - 1).ToString()+" страниц.";
+            if (turn > Convert.ToInt32(numericUpDown3.Value)) {
+                code += "</body></html>";
+                string Date = DateTime.Now.ToString();
+                Date = Date.Replace(":", " ");
+                File.WriteAllText(Date + ".html", code, Encoding.Default);
+                timer1.Enabled = false;
+                listBox1.Items.Add("Обработка завершена.");
+                button1.Enabled = true;
+                numericUpDown1.Enabled = true;
+                numericUpDown2.Enabled = true;
+                numericUpDown3.Enabled = true;
+            }
+        }
+    }
+}
